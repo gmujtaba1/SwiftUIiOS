50cca4bf88bc98eb2f9cfc284539bf0c1bb4e724
Ghulam Mujtaba <ghulam.mujtaba@systemsltd.com>
2021-11-12 10:44:52 +0500

    Initial Commit

---
+++ SwiftUIiOS/SwiftUIiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiOSApp.swift
+//  SwiftUIiOS
+//
+//  Created by Ghulam Mujtaba on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
